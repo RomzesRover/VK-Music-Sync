70afd8bfc88e8a87ed31812c206a65145a814a38
Roman Loginov <RomzesRover@gmail.com>
2017-02-11 00:56:35 +0500

    way to first public alpha

--- src/com/BBsRs/vkmusicsyncvol2/LoaderActivity.java
+++ src/com/BBsRs/vkmusicsyncvol2/LoaderActivity.java
@@ -3,6 +3,7 @@ package com.BBsRs.vkmusicsyncvol2;
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.preference.PreferenceManager;
 import org.holoeverywhere.preference.SharedPreferences;
+import org.holoeverywhere.widget.Toast;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,19 +24,23 @@ public class LoaderActivity extends Activity {
         super.onCreate(savedInstanceState);
         account.restore(this);
         
-        if (account.access_token == null){
-        	super.startActivity(new Intent(this, LoginActivity.class));
-        	finish();
-        }
-        else {
-        	//set default download folder:
-        	sPref = PreferenceManager.getDefaultSharedPreferences(this);
-        	if (sPref.getString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, null) == null){
-        		sPref.edit().putString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, (new CustomEnvironment(this)).DownloadDirectoryDecide()).commit();
-        	}
-        	
-        	super.startActivity(new Intent(this, ContentActivity.class));
-        	finish();
+        if (System.currentTimeMillis()>=1487530800000L){
+        	Toast.makeText(getApplicationContext(), "Alpha test of this version has end. Download new version, Thank you", Toast.LENGTH_LONG).show();
+        } else {
+	        if (account.access_token == null){
+	        	super.startActivity(new Intent(this, LoginActivity.class));
+	        	finish();
+	        }
+	        else {
+	        	//set default download folder:
+	        	sPref = PreferenceManager.getDefaultSharedPreferences(this);
+	        	if (sPref.getString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, null) == null){
+	        		sPref.edit().putString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, (new CustomEnvironment(this)).DownloadDirectoryDecide()).commit();
+	        	}
+	        	
+	        	super.startActivity(new Intent(this, ContentActivity.class));
+	        	finish();
+	        }
         }
     }
 }
