8d2a2547017e36e59dfe2b6763c117408b8e7a2c
Roman Loginov <RomzesRover@gmail.com>
2017-02-18 11:02:05 +0500

    delete determine default directory from loader screen

--- src/com/BBsRs/vkmusicsyncvol2/LoaderActivity.java
+++ src/com/BBsRs/vkmusicsyncvol2/LoaderActivity.java
@@ -1,23 +1,18 @@
 package com.BBsRs.vkmusicsyncvol2;
 
 import org.holoeverywhere.app.Activity;
-import org.holoeverywhere.preference.PreferenceManager;
-import org.holoeverywhere.preference.SharedPreferences;
 import org.holoeverywhere.widget.Toast;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.BBsRs.vkmusicsyncvol2.BaseApplication.Account;
-import com.BBsRs.vkmusicsyncvol2.BaseApplication.Constants;
-import com.BBsRs.vkmusicsyncvol2.BaseApplication.CustomEnvironment;
 
 
 
 public class LoaderActivity extends Activity {
 	
 	Account account = new Account();
-	SharedPreferences sPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +27,6 @@ public class LoaderActivity extends Activity {
 	        	finish();
 	        }
 	        else {
-	        	//set default download folder:
-	        	sPref = PreferenceManager.getDefaultSharedPreferences(this);
-	        	if (sPref.getString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, null) == null){
-	        		sPref.edit().putString(Constants.PREFERENCES_DOWNLOAD_DIRECTORY, (new CustomEnvironment(this)).DownloadDirectoryDecide()).commit();
-	        	}
-	        	
 	        	super.startActivity(new Intent(this, ContentActivity.class));
 	        	finish();
 	        }
