3d8091877c54c3c7f2c27e3c5a4a6994d936d652
Roman Loginov <RomzesRover@gmail.com>
2017-04-24 14:34:31 +0500

    clean

--- src/com/BBsRs/vkmusicsyncvol2/ContentActivity.java
+++ src/com/BBsRs/vkmusicsyncvol2/ContentActivity.java
@@ -25,7 +25,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout.DrawerListener;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
